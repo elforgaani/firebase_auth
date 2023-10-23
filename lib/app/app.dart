44ab38b8a5ec2e867e5705e1de931bd0bda03490
elforgaani <mohamed.elforgani1999@gmail.com>
2023-10-23 19:47:28 +0200

    Add App Folder

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/core/utils/route_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -13,6 +14,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(onGenerateRoute: RouteManager.generateRoute);
   }
 }
