24dd43f22022904dac184cd61454bfde9a1b6b0e
elforgaani <mohamed.elforgani1999@gmail.com>
2023-10-23 19:48:57 +0200

    Add FontManager file

---
+++ lib/core/resources/font_manager.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = '';
+}
+
+class FontWeightManager {
+  static FontWeight thin = FontWeight.w100;
+  static FontWeight extraLight = FontWeight.w200;
+  static FontWeight light = FontWeight.w300;
+  static FontWeight regular = FontWeight.w400;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight extraBold = FontWeight.w800;
+  static FontWeight black = FontWeight.w900;
+}
+
+class FontSize {
+  static double s12 = 12.0;
+  static double s13 = 13.0;
+  static double s14 = 14.0;
+  static double s15 = 15.0;
+  static double s16 = 16.0;
+  static double s17 = 17.0;
+  static double s18 = 18.0;
+  static double s20 = 20.0;
+  static double s22 = 22.0;
+  static double s24 = 24.0;
+  static double s26 = 26.0;
+  static double s28 = 28.0;
+  static double s30 = 30.0;
+  static double s32 = 32.0;
+  static double s36 = 36.0;
+  static double s40 = 40.0;
+  static double s42 = 42.0;
+  static double s44 = 44.0;
+  static double s46 = 46.0;
+  static double s60 = 60.0;
+  static double s96 = 96.0;
+}
