2b52a47bb0c2adc6ba2a9768aeb105543fb60390
elforgaani <mohamed.elforgani1999@gmail.com>
2023-10-23 20:35:38 +0200

    Add ValuesManager file

---
+++ lib/core/resources/values_manager.dart
@@ -0,0 +1,61 @@
+class AppSize {
+  static const double s1 = 1;
+  static const double s2 = 2;
+  static const double s1_5 = 1.5;
+  static const double s8 = 8;
+  static const double s10 = 10;
+  static const double s12 = 12;
+  static const double s14 = 14;
+  static const double s16 = 16;
+  static const double s20 = 20;
+  static const double s24 = 24;
+  static const double s28 = 28;
+  static const double s30 = 30;
+  static const double s32 = 32;
+  static const double s34 = 34;
+  static const double s40 = 40;
+  static const double s45 = 45;
+  static const double s50 = 50;
+  static const double s60 = 60;
+  static const double s70 = 70;
+  static const double s100 = 100;
+  static const double s120 = 120;
+  static const double s140 = 140;
+  static const double s145 = 145;
+  static const double s155 = 155;
+  static const double s160 = 160;
+  static const double s170 = 170;
+  static const double s200 = 200;
+  static const double s220 = 220;
+  static const double s280 = 280;
+  static const double s290 = 290;
+  static const double s300 = 300;
+  static const double s315 = 315;
+  static const double s330 = 330;
+  static const double s400 = 400;
+}
+
+class AppPadding {
+  static const double p8 = 8;
+  static const double p10 = 10;
+  static const double p12 = 12;
+  static const double p14 = 14;
+  static const double p74 = 74;
+  static const double p18 = 18;
+  static const double p16 = 16;
+  static const double p23 = 23;
+  static const double p55 = 55;
+}
+
+class MediaQueryValues {
+  static const double q_1 = 0.1;
+  static const double q_2 = 0.2;
+  static const double q_3 = 0.3;
+  static const double q_4 = 0.4;
+  static const double q_5 = 0.5;
+  static const double q_6 = 0.6;
+  static const double q_7 = 0.7;
+  static const double q_8 = 0.8;
+  static const double q_9 = 0.9;
+  static const double q1 = 1;
+}
