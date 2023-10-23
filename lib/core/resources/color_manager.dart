147acf64ae5aa0fa5f738a6ec0c531f3b2ffea47
elforgaani <mohamed.elforgani1999@gmail.com>
2023-10-23 19:47:09 +0200

    Add ColorManager

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primaryColor = Color(0xFF04284A);
+  static const Color secondaryColor = Color(0x4F224261);
+  static const Color lightScondary = Color(0xFF356899);
+  static const Color grey = Color(0xFFCACBCE);
+  static const Color darkGrey = Color(0xFFAFB0B6);
+  static const Color lightBlue = Color(0xFF2C557D);
+  static const Color textGrey = Color(0xFF95969D);
+  static const Color error = Color(0xffe61f34);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color trasnparent = Colors.transparent;
+}
